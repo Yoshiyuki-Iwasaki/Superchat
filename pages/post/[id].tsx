82dc67c082f1f2ab2ea86e47cdb11ed0bcf8c717
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-11-28 16:43:04 +0900

    add typescript.

---
+++ pages/post/[id].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostDetail = () => {
+  return (
+    <div>投稿詳細</div>
+  )
+}
+
+export default PostDetail;
---
+++ pages/post/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostList = () => {
+  return (
+    <div>投稿一覧</div>
+  )
+}
+
+export default PostList;
