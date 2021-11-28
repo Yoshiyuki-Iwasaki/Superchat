07f8920c93854a371d8334c5772a39ae30dfb2df
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-11-29 02:09:00 +0900

    fix env.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    SUPABASE_URL: "[NEXT_PUBLIC_SUPABASE_URL]",
-    SUPABASE_KEY: "[NEXT_PUBLIC_SUPABASE_KEY]",
-  },
 };
--- src/components/util/supabase.ts
+++ src/components/util/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-export const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-
 export const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_KEY as string
+  "https://ofiqdbpkhdqqlrbrbrkk.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODA5OTc3MiwiZXhwIjoxOTUzNjc1NzcyfQ.p0GeyYmDG_80DEm77-xfoJj8N-8eRWW07oEcp4GYyt8"
 );
