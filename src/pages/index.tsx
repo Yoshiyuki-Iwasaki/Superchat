0f6c29ab086274924327db91c1428b48cfefe6bd
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-11-29 12:11:43 +0900

    create chat function.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState, useEffect} from "react";
 import Link from "next/link";
 import {
   supabase
@@ -6,18 +6,49 @@ import {
 
 export type HomeType = {
   posts: any;
+  chat: any;
 };
 
 const Home: React.FC<HomeType> = ({ posts }) => {
+  const user = supabase.auth.user();
   const [inputData, setInputData] = useState({ message: "" });
+  const [chatList, setChatList] = useState([]);
   const { message } = inputData;
 
+  useEffect(() => {
+    const fetch = async () => {
+      const { data, error }: any = await supabase
+        .from("chat")
+        .select("id, title, created_at")
+        .contains("users", [user.id]);
+      setChatList(data);
+    };
+    fetch();
+  }, []);
+
   const createPost = async () => {
-    await supabase.from("posts").insert([{ message }]).single();
+    await supabase
+      .from("posts")
+      .insert([{ message, user_id: user.id }])
+      .single();
     setInputData({ message: "" });
   };
   return (
     <>
+      <h2>チャット一覧</h2>
+      <ul>
+        {chatList &&
+          chatList.map((chat: any, index: number) => (
+            <li key={index}>
+              <Link href={`chat/${chat.id}`} as={`chat/${chat.id}`} passHref>
+                <a>
+                  <p>{chat.title}</p>
+                </a>
+              </Link>
+            </li>
+          ))}
+      </ul>
+      <h2>メッセージ一覧</h2>
       <ul>
         {posts.data.map((post: any, index: number) => (
           <li key={index}>
