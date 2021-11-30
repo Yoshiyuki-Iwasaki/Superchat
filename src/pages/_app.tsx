7b4a4bb544397a45a136b1c508da731ca8f237e9
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-11-30 22:17:16 +0900

    create auth function.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,44 @@
-import '../../styles/globals.css'
+import { AppProps } from "next/app";
+import { supabase } from "../components//util/supabase";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const { pathname, push } = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  supabase.auth.onAuthStateChange((_, session) => {
+    if (session?.user && (pathname === "/signin" || pathname === "/signup")) {
+      push("/");
+    } else if (!session?.user && pathname !== "/signup") {
+      push("/signin");
+    }
+  });
+
+  useEffect(() => {
+    (async () => {
+      const user = supabase.auth.user();
+      if (user && (pathname === "/signin" || pathname === "/signup")) {
+        await push("/");
+      } else if (!user && pathname !== "/signup") {
+        await push("/signin");
+      }
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <>
+          <button onClick={() => supabase.auth.signOut()}>ログアウト</button>
+          <Component {...pageProps} />
+        </>
+      )}
+    </>
+  );
 }
 
 export default MyApp
