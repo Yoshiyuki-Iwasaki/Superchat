273068f6eb3ebeec084e922770ddfdbd7b4d1616
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-12-01 05:28:41 +0900

    fix profile function.

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { supabase } from "../components/util/supabase";
+import router from "next/router";
 
 const Profile = () => {
   const user = supabase.auth.user();
   const [inputData, setInputData] = useState({ username: "" });
   const { username } = inputData;
+
+    useEffect(() => {
+      const fetch = async () => {
+        const { profileData, profileDataError }: any = await supabase
+          .from("profile")
+          .select("")
+          .eq("id", user.id);
+        if (profileData != undefined) router.push("/");
+      };
+      fetch();
+    }, []);
 
   const createProfile = async (e) => {
     e.preventDefault();
