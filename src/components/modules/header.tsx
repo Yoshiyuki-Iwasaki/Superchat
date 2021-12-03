3a74ffeecfeceb60e12daf9dc0dc8f116b33abdb
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-12-03 12:56:49 +0900

    fix header.

--- src/components/modules/header.tsx
+++ src/components/modules/header.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { supabase } from "../util/supabase";
 import { useRouter } from "next/router";
 
 const Header = () => {
+  const [userList, setUserList] = useState([]);
   const router = useRouter();
   const signOut = () => {
     supabase.auth.signOut();
     router.push("./signin");
-  }
+  };
   const user = supabase.auth.user();
+  useEffect(() => {
+    const fetch = async () => {
+      const { data, error }: any = await supabase
+        .from("users")
+        .select("id, fullname, avatarurl")
+        .contains("users", [user.id]);
+      setUserList(data);
+    };
+    console.log("userList", userList);
+    fetch();
+  }, []);
   return (
     <>
-      <p>{user.email}</p>
-      {/* <button onClick={signOut}>ログアウト</button> */}
+      {/* <p>{user.email}</p> */}
+      <button onClick={signOut}>ログアウト</button>
     </>
   );
-}
+};
 
-export default Header
+export default Header;
