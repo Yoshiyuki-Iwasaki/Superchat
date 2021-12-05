7bf61e25b8c9cba98cd79be73f20246ed0186cd5
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-12-06 02:46:54 +0900

    check.

--- src/components/chat/chatList.tsx
+++ src/components/chat/chatList.tsx
@@ -10,20 +10,24 @@ export type ChatListType = {
 const ChatList: React.FC<ChatListType> = ({ chatData }) => {
   const [posts, setPosts] = useState([]);
 
+  console.log("chatData", chatData);
+
   useEffect(() => {
     setPosts(chatData);
   }, []);
 
+  console.log('posts', posts);
+
   return (
     <List>
-      {/* {posts &&
+      {posts &&
         posts.map((post: any, index: number) => (
           <ListItem key={index}>
             <Date>{formatDate(post.created_at)}</Date>
             <Message>{post.message}</Message>
             <Like id={post.id} />
           </ListItem>
-        ))} */}
+        ))}
     </List>
   );
 };
