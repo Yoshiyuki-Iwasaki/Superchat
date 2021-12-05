e6bd693dafd029733f95109909c880990ff536fb
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-12-06 03:58:51 +0900

    check.

--- src/components/chat/chatList.tsx
+++ src/components/chat/chatList.tsx
@@ -16,6 +16,9 @@ const ChatList: React.FC<ChatListType> = ({ chatData }) => {
   const [inputData, setInputData] = useState({ message: "" });
   const { message } = inputData;
 
+  console.log("posts", posts);
+  console.log("inputData", inputData);
+
   const createPost = async e => {
     if (!message) return;
     e.preventDefault();
@@ -24,6 +27,7 @@ const ChatList: React.FC<ChatListType> = ({ chatData }) => {
         .from("posts")
         .insert([{ message, user_id: user.id, chat_id: chatData.id }])
         .single();
+      fetchPost();
       if (error) throw new Error();
     } catch (error) {
       alert(error.message);
