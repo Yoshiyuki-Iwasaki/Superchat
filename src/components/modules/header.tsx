2e986f2c9dda93a034bbd0b84241a6512322d393
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-12-05 18:52:56 +0900

    check.

--- src/components/modules/header.tsx
+++ src/components/modules/header.tsx
@@ -33,7 +33,7 @@ const Header = () => {
           </Link>
         </Title>
         <RightArea>
-          {userList ? <p>{userList[0].id}</p> : <p>test</p>}
+          {userList[0] ? <p>{userList[0].id}</p> : <p>test</p>}
           <Link href="/editProfile" as="/editProfile" passHref>
             <LinkText>プロフィール編集</LinkText>
           </Link>
