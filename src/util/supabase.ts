39c92aa0125556c7d09c152a0a30b4dabee35b9c
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-12-08 11:59:43 +0900

    fix env.

--- src/util/supabase.ts
+++ src/util/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://ofiqdbpkhdqqlrbrbrkk.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODA5OTc3MiwiZXhwIjoxOTUzNjc1NzcyfQ.p0GeyYmDG_80DEm77-xfoJj8N-8eRWW07oEcp4GYyt8"
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY
 );
