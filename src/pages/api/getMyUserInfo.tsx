9d1ec597762c071ffcaae9f267f49adac27572d1
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-01-23 21:39:00 +0900

    check.

--- src/pages/api/getMyUserInfo.tsx
+++ src/pages/api/getMyUserInfo.tsx
@@ -10,6 +10,7 @@ const getMyUserInfo = async (req: NextApiRequest, res: NextApiResponse) => {
     .eq("id", [user.id]);
   // 401 Unauthorized、認証が必要
   if (error) return res.status(401).json({ error: error.message });
+  console.log("test");
   // 200番台は、処理が成功して正常にレスポンスができている状態
   return res.status(200).json(data);
 };
