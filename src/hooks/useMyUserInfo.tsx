c0e1a7a453a537a841d8f2dcbfb922dfb2835df1
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-01-23 19:50:50 +0900

    fix api.

--- src/hooks/useMyUserInfo.tsx
+++ src/hooks/useMyUserInfo.tsx
@@ -1,10 +1,10 @@
 import useSWR, { SWRConfig } from "swr";
-const fetcher = args => fetch(args).then(res => res.json());
+const fetcher = args => fetch(args).then((res: any) => res.json());
 
 // useAirportという関数をつくる
 export const useMyUserInfo = () => {
   // useSWR(アクセス先,関数,オプション)
-  const { data, error } = useSWR("./pages/api/getMyUserInfo", fetcher);
+  const { data, error } = useSWR("/api/getMyUserInfo", fetcher);
   return {
     MyUserInfoData: data,
     isLoading: !error && !data,
