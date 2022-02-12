8e16e6b9b335cffab1935e6c8a226ca8504f404e
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-12 19:45:54 +0900

    fix profile/edit page.

--- src/pages/profile/edit/presenter.tsx
+++ src/pages/profile/edit/presenter.tsx
@@ -1,25 +1,43 @@
 import React from "react";
 import Layout from "../../../components/templates/layout";
-import { default as Styled } from "./style";
+import styled from "styled-components";
+
+const Main = styled.div``;
+const Title = styled.h2`
+  padding: 15px 0;
+  font-size: 18px;
+  font-weight: 700;
+`;
+const Form = styled.form`
+  margin-top: 15px;
+`;
+const Input = styled.input`
+  font-size: 14px;
+`;
+const Button = styled.button`
+  margin-top: 10px;
+  padding: 5px 10px;
+  background: #2b3a42;
+  font-size: 14px;
+  color: #f3f3f3;
+`;
 
 const Presenter = ({ fullname, UpdateProfile, inputData, setInputData }) => {
   return (
     <Layout>
-      <Styled.Main>
-        <Styled.Title>プロフィール編集</Styled.Title>
-        <Styled.Form onSubmit={e => UpdateProfile(e)}>
-          <Styled.Input
+      <Main>
+        <Title>プロフィール編集</Title>
+        <Form onSubmit={e => UpdateProfile(e)}>
+          <Input
             placeholder="fullname"
             value={fullname}
             onChange={e =>
               setInputData({ ...inputData, fullname: e.target.value })
             }
           />
-          <Styled.Button onClick={e => UpdateProfile(e)}>
-            プロフィール編集
-          </Styled.Button>
-        </Styled.Form>
-      </Styled.Main>
+          <Button onClick={e => UpdateProfile(e)}>プロフィール編集</Button>
+        </Form>
+      </Main>
     </Layout>
   );
 };
