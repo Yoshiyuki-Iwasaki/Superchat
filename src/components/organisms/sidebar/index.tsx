d310c71e850736679383a18a590a008de780b5fe
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-12 18:36:12 +0900

    fix sidebar component.

--- src/components/organisms/sidebar/index.tsx
+++ src/components/organisms/sidebar/index.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../../../util/supabase";
-import Link from "next/link";
 import router from "next/router";
-import { SidebarArea, Title, List, ListItem, LinkText } from "./style";
+import Presenter from "./presenter";
 
 const Sidebar = () => {
   const user = supabase.auth.user();
@@ -18,23 +17,7 @@ const Sidebar = () => {
     };
     fetch();
   }, []);
-  return (
-    <SidebarArea>
-      <Title>チャット一覧</Title>
-      <List>
-        {chatList &&
-          chatList.map((chat: any, index: number) => (
-            <ListItem key={index}>
-              <Link href={`/chat/${chat.id}`} as={`/chat/${chat.id}`} passHref>
-                <LinkText router_id={router.query.id} chat_id={chat.id}>
-                  {chat.title}
-                </LinkText>
-              </Link>
-            </ListItem>
-          ))}
-      </List>
-    </SidebarArea>
-  );
+  return <Presenter chatList={chatList} router={router} />;
 };
 
 export default Sidebar;
