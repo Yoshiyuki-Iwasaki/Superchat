c33d7c72582a29a033eb8b503eb38ff41e71d533
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-12 18:19:22 +0900

    fix profile/edit page.

--- src/pages/profile/edit/index.tsx
+++ src/pages/profile/edit/index.tsx
@@ -1,27 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../../../util/supabase";
-import styled from "styled-components";
 import Layout from "../../../components/layout";
-
-const Main = styled.div``;
-const Title = styled.h2`
-  padding: 15px 0;
-  font-size: 18px;
-  font-weight: 700;
-`;
-const Form = styled.form`
-  margin-top: 15px;
-`;
-const Input = styled.input`
-  font-size: 14px;
-`;
-const Button = styled.button`
-  margin-top: 10px;
-  padding: 5px 10px;
-  background: #2b3a42;
-  font-size: 14px;
-  color: #f3f3f3;
-`;
+import { Main, Title, Form, Input, Button } from "./style";
 
 const EditProfile = () => {
   const user = supabase.auth.user();
---
+++ src/pages/profile/edit/style.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Main = styled.div``;
+export const Title = styled.h2`
+  padding: 15px 0;
+  font-size: 18px;
+  font-weight: 700;
+`;
+export const Form = styled.form`
+  margin-top: 15px;
+`;
+export const Input = styled.input`
+  font-size: 14px;
+`;
+export const Button = styled.button`
+  margin-top: 10px;
+  padding: 5px 10px;
+  background: #2b3a42;
+  font-size: 14px;
+  color: #f3f3f3;
+`;
