07b298082c029f76c15f3a349bf85c305161b1cf
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-02-12 19:46:08 +0900

    fix signin page.

--- src/pages/signin/presenter.tsx
+++ src/pages/signin/presenter.tsx
@@ -1,20 +1,53 @@
 import React from "react";
-import { default as Styled } from "./style";
+import styled from "styled-components";
+
+const Main = styled.div``;
+const Inner = styled.div`
+  margin: 0 auto;
+  position: relative;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
+const ButtonArea = styled.div`
+  padding: 15px 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 2px solid #2b3a42;
+`;
+const Button = styled.button`
+  padding: 15px 20px;
+  display: block;
+  font-size: 14px;
+  color: #f3f3f3;
+  font-weight: 700;
+`;
+
+const GitHubButton = styled(Button)`
+  background: #24292f;
+`;
+
+const TwitterButton = styled(Button)`
+  margin-top: 10px;
+  background: #1da1f2;
+`;
 
 const Presenter = ({ handleGitHubSignIn, handleTwitterSignIn }) => {
   return (
-    <Styled.Main>
-      <Styled.Inner>
-        <Styled.ButtonArea>
-          <Styled.GitHubButton onClick={e => handleGitHubSignIn(e)}>
+    <Main>
+      <Inner>
+        <ButtonArea>
+          <GitHubButton onClick={e => handleGitHubSignIn(e)}>
             GitHubログイン
-          </Styled.GitHubButton>
-          <Styled.TwitterButton onClick={e => handleTwitterSignIn(e)}>
+          </GitHubButton>
+          <TwitterButton onClick={e => handleTwitterSignIn(e)}>
             Twitterログイン
-          </Styled.TwitterButton>
-        </Styled.ButtonArea>
-      </Styled.Inner>
-    </Styled.Main>
+          </TwitterButton>
+        </ButtonArea>
+      </Inner>
+    </Main>
   );
 };
 
