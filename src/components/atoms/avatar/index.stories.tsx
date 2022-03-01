5372c6b3f1d642d38919d9314cdd08886e83d070
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-03-02 00:55:44 +0900

    fix avatar stories file.

--- src/components/atoms/avatar/index.stories.tsx
+++ src/components/atoms/avatar/index.stories.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import { withKnobs } from "@storybook/addon-knobs";
-import Avatar from "./";
+import React from 'react';
+import { withKnobs } from '@storybook/addon-knobs';
+import Avatar from './';
 
-export const basicAvatar = () => (
-  <Avatar userId={`de13f57b-aa19-470c-9ad3-c09f0e6a215b`} />
-);
+export const basicAvatar = () => <Avatar />;
 
 export default {
   component: Avatar,
   decorators: [withKnobs],
-  title: "atoms/Avatar",
+  title: 'atoms/Avatar',
 };
