912d4dea6ee866a90006d352daff80be53089700
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-03-30 01:33:08 +0900

    create sidebar component.

---
+++ src/components/organisms/sidebar/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Presenter from './presenter';
+
+const Sidebar = () => {
+  return <Presenter />;
+};
+
+export default Sidebar;
---
+++ src/components/organisms/sidebar/presenter.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Main } from './style';
+
+const Presenter = () => {
+  return <Main>Sidebar</Main>;
+};
+
+export default Presenter;
